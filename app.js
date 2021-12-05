ebab0dc458b59c3ee715ad7814886928859d0826
YevgeniyBukurIT <85176058+YevgeniyBukurIT@users.noreply.github.com>
2021-12-05 03:48:46 +0200

    Create slow scroll and slider with use jQuery

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 $(function () {
+
+    //Fixed header
     let header = $('#header');
     let intro = $('#intro');
     let introH = intro.innerHeight();
     let scrollPosition = $(window).scrollTop();
 
+    checkScroll(scrollPosition, introH)
 
-
-    $(window).on('scroll load resize', function () {
+    $(window).on('scroll resize', function () {
         introH = intro.innerHeight();
         scrollPosition = $(this).scrollTop();
+
+        checkScroll(scrollPosition, introH)
+    });
+
+    function checkScroll(scrollPosition, introH) {
         if (scrollPosition > introH) {
-            header.addClass('fixed')
+            header.addClass('fixed');
         }
         else {
-            header.removeClass('fixed')
+            header.removeClass('fixed');
         }
-    })
+    }
+
+    //Slow scroll
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elId = $(this).data('scroll');
+        let elOffset = $(elId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: elOffset - 150
+        }, 700);
+
+    });
+
+    //Slider
+    let slider = $('#servicesSlider');
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true,
+    });
 
 
-
-})
+});
